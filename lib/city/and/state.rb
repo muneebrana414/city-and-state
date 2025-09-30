079fe19a1e3425258f510deda222745b5b2f4c97
muneebrana414 <muneebrana414@gmail.com>
2025-09-30 11:53:50 +0500

    Initial commit

---
+++ lib/city/and/state.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "state/version"
+
+module City
+  module And
+    module State
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/city/and/state_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe City::And::State do
+  it "has a version number" do
+    expect(City::And::State::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
